b4e1d9ba07b13f37ce909a7141f230612cc97401
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2019-08-10 16:51:57 +0530

    Add files via upload

---
+++ apc/LinkedList_swapAdjecentRecursion.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h> 
+#include <stdlib.h>
+struct node{
+	int data;
+	struct node* next;
+}; 
+void insertBeg(struct node **h,int val){
+	struct node* n=(struct node*)malloc(sizeof(struct node));
+	n->data=val;
+	n->next=*h;
+	*h=n;
+}
+struct node* swapAdjecentRecursion(struct node *h){
+	struct node* first=h;
+	struct node* second=first->next;
+	if(first->next==NULL){
+		return first;
+	}
+	second->next=swapAdjecentRecursion(second->next);
+	first->next=second->next;
+	second->next=first;
+	return second;
+}
+int main(){
+	struct node* head=NULL;
+	insertBeg(&head,90);
+	insertBeg(&head,10);
+	insertBeg(&head,20);
+	insertBeg(&head,30);
+	insertBeg(&head,40);
+	insertBeg(&head,50);
+	insertBeg(&head,60);
+	insertBeg(&head,70);
+	insertBeg(&head,80);
+	head=swapAdjecentRecursion(head);
+	struct node* curr=head;
+	while(curr!=NULL){
+		printf("%d ",curr->data);
+		curr=curr->next;
+	}	
+	return 0;
+	
+}
