f7e63f7e4620527577e4bf128ec03c7ceaff3cfc
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2019-08-12 16:24:11 +0530

    Add files via upload

---
+++ apc/LinkedList_reverseBetween.cpp
@@ -0,0 +1,37 @@
+listnode* reverseBetween(listnode* A, int B, int C) {
+    listnode* curr=A;
+    listnode* curr1=A;
+    listnode* prev1=A;
+    listnode* prev=NULL;
+    listnode* next1;
+    if(B==C||C<B)
+        return  A;
+    int i=0;
+    int j=0;
+    while(i<C-1){
+        if(j<B-1){
+            prev1=curr;
+            curr=curr->next;
+            j++;
+        }
+        curr1=curr1->next;
+        i++;
+    }
+    prev=curr1->next;
+    int d;
+    while(curr!=NULL&&d<(C-B)+1){
+        next1=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next1;
+        d++;
+    }
+    if(B==1){
+        return curr1;
+    }else{
+        prev1->next=curr1;
+    }
+        
+    return A;
+}
+
