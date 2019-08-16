d63325c92e0f70c90c526749276af1903eab2cab
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2019-08-16 18:53:16 +0530

    Add files via upload

---
+++ apc/stack.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+typedef struct stack{
+	char data;
+	struct stack* next;
+}Stack; 
+
+
+Stack* push(Stack *h,char val){
+	Stack* n=(Stack*)malloc(sizeof(Stack));
+	n->data=val;
+	n->next=h;
+	return n;
+}
+
+void pop(Stack **h){
+	if(*h==NULL){
+		return;
+	}else{
+		Stack* temp=*h;
+		*h=(*h)->next;
+		free(temp);
+		temp=NULL;
+	}
+}
+
+char peak(Stack **h){
+	Stack* curr=*h;
+	return curr->data;
+}
+
+bool isEmpty(Stack **h){
+	if(*h==NULL)
+		return true;
+		
+	return false;
+} 
+
+int main(){
+	Stack* head=NULL;
+	head=push(head,'a');
+	head=push(head,'a');
+	pop(&head);
+	cout<<peak(&head)<<endl;
+	pop(&head);
+	cout<<isEmpty(&head);
+	return 0;
+}
---
+++ apc/stack_equalBrackets.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+
+typedef struct stack{
+	char data;
+	struct stack* next;
+}Stack; 
+
+
+Stack* push(Stack *h,char val){
+	Stack* n=(Stack*)malloc(sizeof(Stack));
+	n->data=val;
+	n->next=h;
+	return n;
+}
+
+void pop(Stack **h){
+	if(*h==NULL){
+		return;
+	}else{
+		Stack* temp=*h;
+		*h=(*h)->next;
+		free(temp);
+		temp=NULL;
+	}
+}
+
+char peak(Stack **h){
+	Stack* curr=*h;
+	return curr->data;
+}
+
+bool isEmpty(Stack **h){
+	if(*h==NULL)
+		return true;
+		
+	return false;
+} 
+
+int main(){
+	Stack* head=NULL;
+	int n;
+	cin>>n;
+	char ch[n];
+	for(int i=0;i<n;i++){
+		cin>>ch[i];
+	}
+	for(int i=0;i<n;i++){
+		if(ch[i]=='('||ch[i]=='{'||ch[i]=='['||ch[i]=='<'){
+			head=push(head,ch[i]);
+		}
+		if((ch[i]==')'&&peak(&head)=='(')||(ch[i]=='}'&&peak(&head)=='{')||(ch[i]==']'&&peak(&head)=='[')){
+			pop(&head);
+		}
+	}
+	if(isEmpty(&head)){
+		cout<<"true";
+	}else{
+		cout<<"false";
+	}
+	return 0;
+}
