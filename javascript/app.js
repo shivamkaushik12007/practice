5986f986e59d31523785dd3fdb7f67801d7ed013
kaushik <shivamkaushik12007@gmail.com>
2019-10-01 23:08:05 +0530

    javascript programs

---
+++ javascript/app.js
@@ -0,0 +1,96 @@
+// function hello(){
+//     console.log('hello world');
+// }
+// hello();
+
+// let a=25;
+// if(true){
+//     let a=35;
+//     console.log(a);
+// }
+// console.log(a);
+ 
+// const abc=[1,2,3,4];
+// abc.push(10);
+// console.log(abc[4]);
+// const a=20;
+// console.log(a);
+
+//// function funcname(args){
+////     body;
+//// }
+//// funcname(); //for calling 
+
+// function abc(a,b,c=30){ //default arguments
+//     console.log(a+" "+b+" "+c);
+// }
+// abc(10,20);
+
+// function abc(...a){ //rest operator
+//     console.log(a);
+// }
+// abc(10,20,'hh');
+
+// var a=function(){ //anonymus function
+//     console.log('hey');
+// }
+// a();
+
+// var hello=(...a)=>{ //arrow functions in ES6
+//     return('helloworld!'+" "+a);
+// }
+// console.log(hello(10,20,30,40));
+
+// console.log(a);
+// var a=10;
+// var b=null;
+// console.log(b);
+
+// var a=[1,2,3,4,5,6];
+// a.push(10);
+// console.log(a[6]);
+// a.pop();
+// console.log(a[5]);
+// a.shift();
+// console.log(a[0]);
+// a.unshift(1);
+// console.log(a[0]);
+
+// var a = function v(){
+//     console.log('hey');
+//     return 100;
+// }
+// // a();
+// console.log(a());
+// function b(c,temp){
+//     console.log(c()+" "+temp);
+// }
+// b(a,10);
+
+// var x=[1,2,3,4,5];
+// function add(number){
+//     return number+=1;
+// }
+// var y=x.map(add);
+// console.log(y);
+// console.log(x);
+
+//  var x=[1,2,3,4,5];
+ // function odd(num){
+//     return num&1;
+// }
+// var y=x.filter(odd);
+// console.log(y);
+
+// x.forEach(function(i){console.log(i);})
+
+//*****(IIFS)immidately invoked function expression
+// (function (){
+//     console.log("hello");
+// })();
+
+// var result=(function (){
+//                 return "hello";
+//             })();
+// console.log(result);
+
