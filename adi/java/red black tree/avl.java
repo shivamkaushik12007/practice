fa9fecbc68230967f33de50b1bab9129a165ff95
kaushik <shivamkaushik12007@gmail.com>
2019-10-22 22:40:04 +0530

    psuedo code inserted in avl

--- adi/java/red black tree/avl.java
+++ adi/java/red black tree/avl.java
@@ -44,3 +44,53 @@ public class LLRBT {
         }
     }
 }
+
+
+
+/*    Node insert(Node node, int key) { 
+  
+        /* 1.  Perform the normal BST insertion */
+        if (node == null) 
+            return (new Node(key)); 
+  
+        if (key < node.key) 
+            node.left = insert(node.left, key); 
+        else if (key > node.key) 
+            node.right = insert(node.right, key); 
+        else // Duplicate keys not allowed 
+            return node; 
+  
+        /* 2. Update height of this ancestor node */
+        node.height = 1 + max(height(node.left), 
+                              height(node.right)); 
+  
+        /* 3. Get the balance factor of this ancestor 
+              node to check whether this node became 
+              unbalanced */
+        int balance = getBalance(node); 
+  
+        // If this node becomes unbalanced, then there 
+        // are 4 cases Left Left Case 
+        if (balance > 1 && key < node.left.key) 
+            return rightRotate(node); 
+  
+        // Right Right Case 
+        if (balance < -1 && key > node.right.key) 
+            return leftRotate(node); 
+  
+        // Left Right Case 
+        if (balance > 1 && key > node.left.key) { 
+            node.left = leftRotate(node.left); 
+            return rightRotate(node); 
+        } 
+  
+        // Right Left Case 
+        if (balance < -1 && key < node.right.key) { 
+            node.right = rightRotate(node.right); 
+            return leftRotate(node); 
+        } 
+  
+        /* return the (unchanged) node pointer */
+        return node; 
+    } 
+  */
