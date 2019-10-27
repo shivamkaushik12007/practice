ea8d9a95d84d34e9d2ce39e83e462942aed64b6e
kaushik <shivamkaushik12007@gmail.com>
2019-10-27 23:22:19 +0530

    qwerty

---
+++ java/faltu.java
@@ -0,0 +1,22 @@
+package com.kaushik;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        int x= scanner.nextInt();
+        //2
+        // scanner.nextLine();
+        int y = scanner.nextInt();
+        scanner.close();
+        System.out.println(x + y +s);
+
+    }
+
+
+
+
+}
