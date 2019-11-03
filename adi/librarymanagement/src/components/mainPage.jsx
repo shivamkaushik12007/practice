0800b4af60dfe3793aeac3f5389d8ed81a632824
kaushik <shivamkaushik12007@gmail.com>
2019-11-03 20:27:44 +0530

    json server starting

--- adi/librarymanagement/src/components/mainPage.jsx
+++ adi/librarymanagement/src/components/mainPage.jsx
@@ -1,13 +1,36 @@
-// import React,{Component} from 'react';
-import {Component} from 'react';
+import React,{Component} from 'react';
+// import {Component} from 'react';
 // import RecentBooksList from './recentBooksList'
 
 class MainPage extends Component{
+    constructor(){
+        super();
+        console.log("constructor is called");
+    }
+
+    componentDidMount(){
+        console.log("component is mounted");
+        fetch('http://localhost:3000/bookList')
+        .then(res=>{
+            return res.json();
+        })
+        .then(res=>{
+            console.log(JSON.stringify(res));
+            this.setState({bookList:res});
+            console.log(this.state.bookList);
+        })
+    }
+    state={
+        bookList:[]
+    }
     render(){
         // return <React.Fragment className="row">
         //     <RecentBooksList/>
         //</React.Fragment>
-          return "hello"  
+        //   return "hello"  /
+        return(
+            "hello"
+        )
     }
 }
 
