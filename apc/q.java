16686d71d7509bad41098154a47b92d2be4d8b6f
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2019-12-18 22:04:01 +0530

    Update q.java

--- apc/q.java
+++ apc/q.java
@@ -1,2 +1,19 @@
-
-stack in queue
+public class q{
+	public void main(String args[]){
+		Queue<Integer> q=new Queue();
+		Scanner scanner=new Scanner(System.in);
+		int n=scanner.nextInt();
+		for(int i=0;i<n;i++){
+			q.enqueue(scanner.nextInt());
+		}
+		Stack<Integer> s=new Stack<>();
+		while(!q.isEmpty()){
+			s.push(q.dequeue);
+		}
+		for(int i=0;i<s.size();i++){
+			int n1=s.peek();
+			q.enqueue(n1);
+			s.pop();
+		}
+	}
+}
