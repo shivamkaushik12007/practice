0b536947b7e95ad8ced9bfa4d3650f933d4270ec
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2019-12-22 23:18:35 +0530

    Create jksk.java

---
+++ debug/jksk.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public void arrange(ArrayList<Integer> a) {
+        int k=Math.power(10,a.size());
+        for(int i=0;i<a.size();i++){
+            if(a.get(i)>=n){
+                int m=a.get(i);
+            }else{
+                a.set(a.get(i),a.get(i));
+            }
+        }
+    }
+}
