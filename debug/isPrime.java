094eadd5be2bd093ef217d61550e6115eab30825
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2020-01-26 18:11:19 +0530

    overloading not allowed

---
+++ debug/isPrime.java
@@ -0,0 +1,53 @@
+
+// import static java.lang.System.in;
+// class Prime{
+//     public void checkPrime(int a){
+//         int l=primeo(a);
+//         if(l==1){
+//             System.out.println(a);
+//         }
+//     }
+//     public void checkPrime(int a,int b){
+//         int arr[]=new int[2];
+//         arr[0]=primeo(a);
+//         arr[1]=primeo(b);
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]>0)
+//             System.out.print(arr[i]+" ");
+            
+//         }
+//         System.out.println();
+//     }
+//     public void checkPrime(int a,int b,int c){
+//         int arr[]=new int[3];
+//         arr[0]=primeo(a);
+//         arr[1]=primeo(b);
+//         arr[2]=primeo(c);
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]>0)
+//             System.out.print(arr[i]+" ");
+//         }
+//         System.out.println();
+//     }
+//     public void checkPrime(int a,int b,int c,int d,int e){
+//         int arr[]=new int[5];
+//         arr[0]=primeo(a);
+//         arr[1]=primeo(b);
+//         arr[3]=primeo(d);
+//         arr[4]=primeo(e);
+//         arr[2]=primeo(c);
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]>0)
+//             System.out.print(arr[i]+" ");
+//         }
+//         System.out.println();
+//     }
+//     public int primeo(int k){
+//         int n=(int)Math.sqrt(k);
+//         for(int i=0;i<n;i++){
+//             if(k%n==0)
+//                 return 0;
+//         }
+//         return k;
+//     }
+// }
