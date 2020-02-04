ae7e53457f9ac0888dde66e766517bf6a47f6df8
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2020-02-04 21:17:14 +0530

    error terminated!

--- debug/sortedCLLInsert.c
+++ debug/sortedCLLInsert.c
@@ -1,6 +1,33 @@
+// void sortedInsert(struct node** head_ref, struct node* new_node){
+//     //complete the code
+
+//     struct node* temp=*head_ref;
+//     if(temp->data>new_node->data){
+//     struct node* temp1=*head_ref;
+//         while(!(temp1->next==temp)){
+//             temp1=temp1->next;
+//         }
+//         new_node->next=temp;
+//         temp1->next=new_node;
+//         (*head_ref)=new_node;
+//         return;
+//     }
+//     while(temp->next->data<new_node->data){
+//         temp=temp->next;
+//     }
+//     new_node->next=temp->next;
+//     temp->next=new_node;
+// }
+
+
+
 void sortedInsert(struct node** head_ref, struct node* new_node){
     //complete the code
-
+    if(*head_ref==NULL){
+        *head_ref=new_node;
+        new_node->next=new_node;
+        return;
+    }
     struct node* temp=*head_ref;
     if(temp->data>new_node->data){
     struct node* temp1=*head_ref;
@@ -12,8 +39,16 @@ void sortedInsert(struct node** head_ref, struct node* new_node){
         (*head_ref)=new_node;
         return;
     }
-    while(temp->next->data<new_node->data){
+    if(temp->next==temp){
+        temp->next=new_node;
+        new_node->next=temp;
+        return;
+    }
+    struct node* temp1=*head_ref;
+    while(temp->next->data<new_node->data&&temp->next!=temp1){
+        // printf("%d",temp->data);
         temp=temp->next;
+        
     }
     new_node->next=temp->next;
     temp->next=new_node;
