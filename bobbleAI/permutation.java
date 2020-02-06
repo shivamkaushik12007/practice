b976bbcab51a3d9daa73322f4737efc8a57e5740
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2020-02-06 19:56:23 +0530

    Bobble.AI internship permutation question

---
+++ bobbleAI/permutation.java
@@ -0,0 +1,49 @@
+//Run file By commands
+//javac permutation.java
+//java Permutation
+//the program is taking input from input.csv file present in same folder as of file.
+
+
+import java.util.*;
+import java.lang.*;
+public class Permutation{
+    ArrayList<String> res=new ArrayList<>();
+	public static void main(String[] args){
+	    String csvFile = "input.csv";
+        String line = "";
+        ArrayList<ArrayList<String>> arr=new ArrayList<>();
+		try{
+		    BufferedReader br=new BufferedReader(new FileReader(csvFile));
+            while ((line=br.readLine())!=null) {
+                char[] inp=line.split(",");
+                ArrayList<String> temp=new ArrayList<>();
+                for(int i=0;i<inp.length;i++){
+                    temp.add(Character.toString(inp[i]));
+                }
+                arr.add(temp);
+            }
+            permutationCheck(arr,0,"");
+            for(int i=0;i<res.size()-1;i++){
+                System.out.print(res.get(i)+", ");
+            }
+            System.out.print(res.get(res.size()-1));
+		}catch(IOException e){
+		    e.printStackTrace();
+		}
+	}
+	public static void permutationCheck(ArrayList<ArrayList<Integer>> arr,int level,String s){
+	    if(level==arr.size()){
+	        return;
+	    }
+	    s+=arr.get(level).get(0);
+	    for(int i=0;i<arr.get(level).size();i++){
+	        s=s.substring(0,s.length()-1);
+	        s+=arr.get(level).get(i);
+	        if(level==arr.size()-1){
+	            res.add(s);
+	        }else{
+	            permutationCheck(arr,level+1,s);
+	        }
+	    }
+	}
+}
