623a6f563dcadea0945343d9dbc179932778745d
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2020-02-13 23:18:33 +0530

    Update Main.java

--- adi/java/LinkedList/Main.java
+++ adi/java/LinkedList/Main.java
@@ -5,16 +5,21 @@ public class Main {
 //        LLRBT lb=new LLRBT();
 //        BST b=new BST();
 //
-
+        LL<Integer> t=new LL();
+        t.append(1);
+        t.append(2);
+        t.append(3);
+        t.append(4);
+        t.print();
 //        System.out.println(b.search(10000));
 
 //        System.out.println(b.height());
 //        LinkedList l=new LinkedList();
-        BST1 l=new BST1();
-        for(int i=5;i>0;i--){
-            l.insert(Integer.toString(i));
-        }
-        System.out.println(l.height());
+//         BST1 l=new BST1();
+//         for(int i=5;i>0;i--){
+//             l.insert(Integer.toString(i));
+//         }
+//         System.out.println(l.height());
         //        System.out.println();
 //        l.delete();
 //        l.delete();
