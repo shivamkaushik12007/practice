49aa28050e93fd70828a63aceeada1963a2c822e
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2020-02-20 21:06:59 +0530

    Update bstInsert.cpp

--- apc/bstInsert.cpp
+++ apc/bstInsert.cpp
@@ -18,7 +18,7 @@ struct node* insert(struct node* h,int data){
 		if(h==NULL){
 			return newNode(data);
 		}
-		
+		//for handling multiple duplicate value if(h->data => data)
 		if(h->data > data)
 			h->left=insert(h->left,data);
 		if(h->data < data) 
