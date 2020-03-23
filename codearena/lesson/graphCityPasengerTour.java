3d08a713b59246f49c42fa19415eb5bc2ae34d04
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2020-03-23 15:45:58 +0530

    Create graphCityPasengerTour.java

---
+++ codearena/lesson/graphCityPasengerTour.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.lang.*;
+class TestClass {
+    Map<String,Integer> res=new HashMap<>();
+    int resMin=Integer.MIN_VALUE;
+    public static void main(String args[] ) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int r=sc.nextInt();
+        ArrayList<Map<Integer,Integer>> arr=new ArrayLIst<>(n);
+        for(int i=0;i<r;i++){
+            int n1=sc.nextInt();
+            int n2=sc.nextInt();
+            int n3=sc.nextInt();
+            arr.get(n1).put(n2,n3);
+            arr.get(n2).put(n1,n3);
+        }
+        int s=sc.nextInt();
+        int d=sc.nextInt();
+        int p=sc.nextInt();
+        int vis[]=new int[n];
+        dfs(s,d,arr,vis,Integer.MAX_VALUE,"");
+        ArrayList<String> result=new ArrayList<>();
+        for(Map.entry<String,Integer> entry:res.entrySet()){
+            if(mp.getValue()==resMin){
+                result.add(mp.getKey());
+            }
+        }
+        Collections.sort(result);
+        String ress=result.get(0);
+        for(int i=0)
+    }
+    public static void dfs(int s,int d,ArrayList<Map<Integer,Integer>> arr,int[] vis,int min,String si){
+        si+=Integer.toString(s);
+        if(s==d){
+            res.put(si,min);
+            if(min<resMin){
+                resMin=min;
+            }
+            return;
+        }
+        if(vis[s]!=1){
+            vis[s]=1;
+            for(Map.entry<Integer,Integer> mp:arr.get(s).entrySet()){
+                if(mp.getValue()<min){
+                    dfs(mp.getKey(),d,arr,vis,mp.getValue(),si);
+                }else{
+                    dfs(mp.getKey(),d,arr,vis,min,si);
+                }
+            }
+        }
+    }
+}
