a25a3d089b31fffb8e72ff2b03a4ecc54aad5537
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2020-04-07 15:27:25 +0530

    fuddu

---
+++ codearena/lesson/sheldonandcab.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner scanner=new Scanner(System.in);
+        int t=scanner.nextInt();
+        while(t-->0){
+            int a=scanner.nextInt();
+            int b=scanner.nextInt();
+            for(int i=0;i<b;i++){
+                int n1=scanner.nextInt();
+                int n2=scanner.nextInt();
+            }
+                System.out.println(a-1);   
+        }
+    }
+}
