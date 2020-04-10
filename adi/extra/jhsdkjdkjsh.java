1289fa263df3464e2b52232b459fed9b7075f2b4
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2020-04-10 14:44:16 +0530

    Create jhsdkjdkjsh.java

---
+++ adi/extra/jhsdkjdkjsh.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int q=sc.nextInt();
+        for(int )
+	}
+}
