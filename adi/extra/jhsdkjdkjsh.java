9451c64462797a67cdab2f677c854ad90c381c09
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2020-04-11 14:37:51 +0530

    Update jhsdkjdkjsh.java

--- adi/extra/jhsdkjdkjsh.java
+++ adi/extra/jhsdkjdkjsh.java
@@ -8,3 +8,26 @@ public class Main
         for(int )
 	}
 }
+
+import java.util.*;
+import java.lang.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        // int k=a<b?a:b;
+        // int cnt=0;
+        // for(int i=0;i<=k;i++){
+        //     if(i*2<c){
+        //         cnt++;
+        //     }else{
+        //         break;
+        //     }
+        // }
+        // k++;
+        System.out.println("1/1");
+    }
+}
+
