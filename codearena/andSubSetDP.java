5811dde4c2529f59130b4dbfef6d9cf78067bf14
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2020-06-01 23:10:55 +0530

    Create andSubSetDP.java

---
+++ codearena/andSubSetDP.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner s = new Scanner(System.in);
+        int t=s.nextInt();
+        while(t-->0){
+            int z=s.nextInt();
+            int n=s.nextInt();
+            int[] arr=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=s.nextInt();   
+            }
+            if(check(z,n,arr)){
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+        }
+    }
+    public static boolean check(int z,int n,int[] arr){
+        for(int i=0;i<n;i++){
+            int k=z&arr[i];
+            z=k<z?k:z;
+            if(z==0) return true;
+        }
+        return false;
+    }
+}
