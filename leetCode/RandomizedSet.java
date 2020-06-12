e19935c4cbfdf1be041ee27ea66126c693432be0
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2020-06-12 22:05:11 +0530

    Create RandomizedSet.java

---
+++ leetCode/RandomizedSet.java
@@ -0,0 +1,37 @@
+class RandomizedSet {
+
+    /** Initialize your data structure here. */
+    ArrayList<Integer> arr;
+    Random rand;
+    public RandomizedSet() {
+        arr=new ArrayList<Integer>();
+        rand = new Random();
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if(arr.contains(val))return false;
+        arr.add(val);
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if(!arr.contains(val))return false;
+        arr.remove(new Integer(val));
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return arr.get(rand.nextInt(arr.size()));
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
