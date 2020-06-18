28e00f60dd069f80195a083438ab08be5e229cf1
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2020-06-18 22:57:39 +0530

    Create logo.py

---
+++ python/practice/incomplete/logo.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+import operator
+
+
+if __name__ == '__main__':
+    s = input()
+    arr=Counter(list(s))
+    x = sorted(arr.items(), key=operator.itemgetter(0))
+    print(x)
+    x = sorted(arr.items(), key=operator.itemgetter(1), reverse=True)
+    print(x)
+    p=len(x) if(len(x)<3) else 3
+    k=0
+    for i in x:
+        if(k>=p):
+            break
+        print(i[0]+" "+str(i[1]))
+        k+=1
