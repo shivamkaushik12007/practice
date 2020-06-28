107598abc63d04ba5b36a9e9aa82f0f16270d32f
Shivam Kaushik <51492267+shivamkaushik12007@users.noreply.github.com>
2020-06-28 23:26:48 +0530

    Update ancestorColorTree.py

--- codearena/lesson/ancestorColorTree.py
+++ codearena/lesson/ancestorColorTree.py
@@ -1,6 +1,16 @@
+def check(arr,brr,x,p):
+    if(x==-1 or p==-1):
+        return -1
+    if(brr[x]==brr[p] and x!=p):
+        return x+1
+    return check(arr,brr,arr[x],p)
+
 n,m=input().split(" ")
-arr=list(map(int,input().split(" ")))
+arr1=list(map(int,input().split(" ")))
 brr=list(map(int,input().split(" ")))
-print("-1",end=" ")
-for i in range(int(n)-1):
-    print()
+# print("-1",end=" ")
+arr=[-1]
+for i in arr1:
+    arr.append(i-1)
+for i in range(int(n)):
+    print(check(arr,brr,i,i),end=" ")
